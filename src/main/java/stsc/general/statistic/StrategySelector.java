66145717e879cde19335bd119a3de07c732826f0
Ivan Sidarau <ivan.sidarau@gmail.com>
2015-05-20 07:56:15 +0200

    #3 added javadocs for StrategySelector

--- src/main/java/stsc/general/statistic/StrategySelector.java
+++ src/main/java/stsc/general/statistic/StrategySelector.java
@@ -5,11 +5,18 @@ import java.util.Optional;
 
 import stsc.general.strategy.TradingStrategy;
 
+/**
+ * {@link StrategySelector} interface is an interface for controlling
+ * collections of {@link TradingStrategy}.<br/>
+ * Common interface to select strategies from different {@link TradingStrategy}
+ * searchers. Be careful and implement multi-thread protected Strategy
+ * Selectors.
+ */
 public interface StrategySelector {
 
 	public Optional<TradingStrategy> addStrategy(final TradingStrategy strategy);
 
-	public void removeStrategy(TradingStrategy strategy);
+	public void removeStrategy(final TradingStrategy strategy);
 
 	public List<TradingStrategy> getStrategies();
 
